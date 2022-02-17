c5b947def11449abade443a981b8b87ebc34154d
Manuehh <manuel.ortega@gmail.com>
2022-02-17 01:54:14 +0100

    semi clean code

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
 var bd;
+let lvl = 1;
+
+const pokemonImage = document.getElementById("card-img-top");
+const pokemonName = document.getElementsByClassName("card-title")[0];
+const pokemonDescription =
+    document.getElementsByClassName("card-description")[0];
+const pokemonAbility = document.getElementsByClassName("card-hability")[0];
+const pokemonType = document.getElementsByClassName("card-type")[0];
+const pokemonType2 = document.getElementsByClassName("card-type2")[0];
+const trainerLevel = document.getElementsByClassName("lvl")[0];
 
 function iniciar() {
+    getNewPokemon()
 
-    var solicitud = indexedDB.open("mibase2");
+    var solicitud = indexedDB.open("PokemonDatabase");
 
     solicitud.onerror = function(e) {
         console.log("error: ", e.target.result);
@@ -10,49 +21,46 @@ function iniciar() {
 
     solicitud.onsuccess = function(e) {
         bd = e.target.result;
-    }
+    };
 
     solicitud.onupgradeneeded = function(e) {
         bd = e.target.result;
         bd.createObjectStore("pokemon", { keyPath: "clave" });
-    }
-
+    };
 }
 
-const pokemonName = document.getElementsByClassName("card-title")[0];
-const pokemonDescription = document.getElementsByClassName("card-description")[0];
-const pokemonAbility = document.getElementsByClassName("card-hability")[0];
-const pokemonType = document.getElementsByClassName("card-type")[0];
-const pokemonType2 = document.getElementsByClassName("card-type2")[0];
-const pokemonImage = document.getElementById("card-img-top");
-
-let lvl = 1;
 const getNewPokemon = () => {
     const randomNumber = getRandomInt(898);
     const shinyChance = getRandomInt(4096);
 
     const url = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+    getShowData(url, shinyChance);
+};
+
+function getShowData(url, shinyChance) {
     fetch(url, {
-            method: 'get',
+            method: "get",
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-
-            let trainerLevel = document.getElementsByClassName("lvl")[0];
-            trainerLevel.textContent = lvl;
-
-            pokemonImage.style.backgroundImage = "url(assets/backgrounds/" + data.types[0].type.name + ".jpg)"
-            pokemonImage.src = getPokemonImageChekingIfShiny(shinyChance, data);
-            pokemonName.textContent = formatFirstLetter(data.name);
-            pokemonDescription.textContent = "Base Experience: " + data.base_experience;
-            pokemonAbility.textContent = "Primary Ability: " + formatFirstLetter(data.abilities[0].ability.name);
-            pokemonType.textContent = "Type: " + formatFirstLetter(data.types[0].type.name);
-            pokemonType2.textContent = getPokemonTypes(data);
-
-
-
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log(data);
+            setValuesOnHTML(data, shinyChance);
         });
+}
+
+function setValuesOnHTML(data, shinyChance) {
+    trainerLevel.textContent = lvl;
+    pokemonImage.style.backgroundImage =
+        "url(assets/backgrounds/" + data.types[0].type.name + ".jpg)";
+    pokemonImage.src = getPokemonImageChekingIfShiny(shinyChance, data);
+    pokemonName.textContent = formatFirstLetter(data.name);
+    pokemonDescription.textContent =
+        "Base Experience: " + data.base_experience;
+    pokemonAbility.textContent =
+        "Primary Ability: " + formatFirstLetter(data.abilities[0].ability.name);
+    pokemonType.textContent =
+        "Type: " + formatFirstLetter(data.types[0].type.name);
+    pokemonType2.textContent = getPokemonTypes(data);
 }
 
 function getPokemonImageChekingIfShiny(shinyChance, data) {
@@ -76,19 +84,14 @@ function getPokemonTypes(data) {
 }
 
 function getRandomInt(range) {
-    return Math.floor((Math.random() * (range)) + 1);
+    return Math.floor(Math.random() * range + 1);
 }
-getNewPokemon()
-
-
 
 function agregarobjeto() {
-
     var transaccion = bd.transaction(["pokemon"], "readwrite");
-
     var almacen = transaccion.objectStore("pokemon");
 
-    console.log("Almacenando..." + pokemonName.textContent)
+    console.log("Almacenando... " + pokemonName.textContent);
 
     almacen.add({
         clave: pokemonName.textContent,
@@ -96,43 +99,32 @@ function agregarobjeto() {
         description: pokemonDescription.textContent,
         ability: pokemonAbility.textContent,
         type1: pokemonType.textContent,
-        type2: pokemonType2.textContent
+        type2: pokemonType2.textContent,
     });
 
-    mostrar()
+    mostrar();
 }
 
 function mostrar() {
-
     var transaccion = bd.transaction(["pokemon"], "readonly");
-
     var almacen = transaccion.objectStore("pokemon").getAll();
-
-    // var cursor = almacen.openCursor();
-    //console.log("Almacen...", almacen)
 
     almacen.onsuccess = function(event) {
         console.log(event.target.result);
+        getNewPokemon();
     };
-
-    // cursor.addEventListener("success", mostrarDatos, false);
-
 }
 
-function mostrarDatos(e) {
+// function mostrarDatos(e) {
+//     var cursor = e.target.result;
 
-    var cursor = e.target.result;
+//     // if (cursor) {
 
-    console.log(cursor)
+//     //     zonadatos.innerHTML += "<div>" + cursor.value.clave + " - " + cursor.value.titulo + " - " + cursor.value.Fecha + "</div>";
 
-    // if (cursor) {
+//     //     cursor.continue();
 
-    //     zonadatos.innerHTML += "<div>" + cursor.value.clave + " - " + cursor.value.titulo + " - " + cursor.value.Fecha + "</div>";
-
-    //     cursor.continue();
-
-    // }
-
-}
+//     // }
+// }
 
 window.addEventListener("load", iniciar, false);
